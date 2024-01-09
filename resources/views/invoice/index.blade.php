e4eec9a90d6525104230028115ef30741db18a34
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-01-09 18:35:45 +0700

    penambahan url tambah invoice

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -4,7 +4,7 @@
 <header class="w-full p-4 border border-gray-200 shadow rounded-lg mb-6">
     <div class="flex justify-between items-center">
         <h1 class="text-2xl">List Invoice</h1>
-        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2">Tambah Invoice</a>
+        <a href="{{ route('invoice.create') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2">Tambah Invoice</a>
     </div>
 </header>
 <main>
