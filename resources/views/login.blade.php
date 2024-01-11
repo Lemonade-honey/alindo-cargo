7391b513d3b9b760bec3f3bf39d9c52ca8573645
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-01-11 19:22:09 +0700

    MEnggunakan tamplate sendiri

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,27 +1,35 @@
-@extends('layout.guest')
-
-@section('body')
-<div class="min-h-[95vh] flex flex-col gap-3 items-center justify-center">
-    <div class="block max-w-sm w-full p-6 bg-white border border-gray-200 rounded-lg shadow">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Login</h5>
-
-        @include('include.flash')
-
-        <form method="post">
-            @csrf
-            <div class="mb-6">
-                <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Username</label>
-                <input type="text" name="email" id="username" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500">
-            </div>
-            <div class="mb-6">
-                <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                <input type="password" name="password" id="username" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500">
-            </div>
-            <div class="flex justify-center">
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Login</button>
-            </div>
-        </form>
-        
-    </div>    
-</div>
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Login</title>
+</head>
+<body>
+    <div class="min-h-[95vh] flex flex-col gap-3 items-center justify-center">
+        <div class="block max-w-sm w-full p-6 bg-white border border-gray-200 rounded-lg shadow">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Login</h5>
+    
+            @include('include.flash')
+    
+            <form method="post">
+                @csrf
+                <div class="mb-6">
+                    <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Username</label>
+                    <input type="text" name="email" id="username" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500">
+                </div>
+                <div class="mb-6">
+                    <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                    <input type="password" name="password" id="username" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500">
+                </div>
+                <div class="flex justify-center">
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Login</button>
+                </div>
+            </form>
+            
+        </div>    
+    </div>
+</body>
+</html>
